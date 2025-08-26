c4e890175d4df46536c95960e7e62bb8a3d7c33c
Panchmore <francisca.monroy@genstudents.org>
2025-08-26 14:29:07 +0000

    Laboratio Completado

--- task4.js
+++ task4.js
@@ -1,7 +1,18 @@
 export class Player {
-  constructor() {
-   
+  constructor(nombre, nivel) {
+    this.nombre = nombre;
+    this.nivel = nivel;
   }
-  
-  
+
+  info() {
+    console.log(`${this.nombre} ha alcanzado el nivel ${this.nivel}!`);
+  }
+
+  levelUp() {
+    this.nivel += 1;
+  }
 }
+const jugador1 = new Player("Javiera", 6);
+jugador1.info();   
+jugador1.levelUp();
+jugador1.info();   
